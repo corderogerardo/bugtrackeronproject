60b8343eb5fdfb905190ff4d23da25f9ccc6390d
Gerardo Cordero <cordero.gerard@gmail.com>
2016-06-13 23:17:35 -0400

    Agregue propel, configure la base de datos mysql para poder crear migraciones, modelos, con propel init, elimine la carpeta vendor que agregue por error, y la carpeta .idea que subi a github

--- public/index.php
+++ public/index.php
@@ -5,12 +5,19 @@
  * Date: 11/06/16
  * Time: 08:13 PM
  */
+
+if(PHP_SAPI == 'cli-server'){
+    $file = __DIR__ . $_SERVER['REQUEST_URI'];
+    IF(is_file($file)){
+        return false;
+    }
+}
+require __DIR__ . '/../vendor/autoload.php';
 session_start();
-require '../vendor/autoload.php';
-$config = require '../app/config/config.php';
+$config = require __DIR__ . '/../app/config/config.php';
 
 $app = new \Slim\App($config);
 
-require '../app/routes/routes.php';
+require __DIR__ . '/../app/routes/routes.php';
 
 $app->run();
